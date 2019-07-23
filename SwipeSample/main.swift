388b09155e679e71f4f2dc21b4b23ba341c4ec4e
Akito Ibaraki <ibaraki.akito.jul20@gmail.com>
2019-07-24 00:35:40 +0900

    bundleIdの修正など

--- SwipeSample/main.swift
+++ SwipeSample/main.swift
@@ -1,14 +1,7 @@
 import Foundation
 import UIKit
 
-if NSClassFromString("XCTestCase") != nil {
-    UIApplicationMain(CommandLine.argc,
-                      CommandLine.unsafeArgv,
-                      NSStringFromClass(UIApplication.self),
-                      NSStringFromClass(TestingAppDelegate.self))
-} else {
     UIApplicationMain(CommandLine.argc,
                       CommandLine.unsafeArgv,
                       NSStringFromClass(UIApplication.self),
                       NSStringFromClass(AppDelegate.self))
-}
